ff9048210132207e2024dca647d9585ae4bed6bc
Chethan616 <chethankrishna2022@gmail.com>
2026-05-22 20:16:03 +0530

    stable

--- essentials-main/app/src/main/java/com/sameerasw/essentials/domain/model/NotificationLightingSide.kt
+++
@@ -1,8 +0,0 @@
-package com.sameerasw.essentials.domain.model
-
-enum class NotificationLightingSide {
-    LEFT,
-    TOP,
-    RIGHT,
-    BOTTOM
-}
--- essentials-main/app/src/main/java/com/sameerasw/essentials/domain/model/NotificationLightingStyle.kt
+++
@@ -1,7 +0,0 @@
-package com.sameerasw.essentials.domain.model
-
-enum class NotificationLightingStyle {
-    STROKE,
-    GLOW,
-    INDICATOR
-}
